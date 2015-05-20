c6a761042b8d386bebdf7712c419f13c5ab6765e
iantsai <iantsai86@gmail.com>
2015-05-20 11:09:53 -0700

    Initial Commit

---
+++ Every.Do/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Every.Do
+//
+//  Created by Ian Tsai on 2015-05-20.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Every.Do/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Every.Do
+//
+//  Created by Ian Tsai on 2015-05-20.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
